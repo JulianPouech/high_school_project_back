4159a6814ad31d4d02efef735f48d5d492900ba5
Julian <pouech.julian@orange.fr>
2024-11-26 00:25:01 +0100

    [Wip]

---
+++ app/Models/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Role extends Model
+{
+  protected $fillable = [
+    'name'
+  ]; 
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,27 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        DB::table('roles')->insert([
+            'name' => 'customer'
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'admin'
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Customer User',
+            'email' => 'customer@example.com',
+            'password' => Hash::make('pwd2019'),
+            'role_id' => 0
         ]);
+
+        User::factory()->create([
+            'name' => 'admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('pwd2019'),
+            'role_id' => 1
+        ]);
+
     }
 }
