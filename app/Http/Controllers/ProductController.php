05e6e4d12a0237cd9c8aaa2631153ef6d7bebfb0
Julian <pouech.julian@orange.fr>
2024-12-12 19:33:59 +0100

    [wip]

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+
 
 class ProductController extends Controller
 {
-    //
+    public function create(): JsonResponse
+    {
+        return response()->json(["status" => "ok"]);
+    }
+
+    public function index(): JsonResponse {
+        return response()->json(Product::all());
+    }
+
+    public function upload(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => ['string','max:255'],
+            'price' => ['integer'],
+            'description' => ['string','max:526']
+        ]);
+        return response()->json($validate);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/product', [ProductController::class, 'upload']);
+Route::get('/product', [ProductController::class, 'index']);
